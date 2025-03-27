881de461001b26f42b86951221ddf04eb00d735f
William Antwi-Boasiako <william.antwi-boasiako@persol.net>
2025-03-27 17:09:30 +0000

    no change

--- config/Config.php
+++ config/Config.php
@@ -76,8 +76,8 @@ class Config
         ];
 
         if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
-             header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-            //header("Access-Control-Allow-Origin: *");
+             //header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header("Access-Control-Allow-Origin: *");
             header('Access-Control-Allow-Credentials: true');
             header("Access-Control-Allow-Headers: Content-Type, Authorization");
             header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
